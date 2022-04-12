33ea6f5bdca09dba01f1122cae593c8223a1ccd0
Kras13 <72404532+Kras13@users.noreply.github.com>
2022-04-13 02:07:41 +0300

    Add DealerServiceTest.

---
+++ CarRentingSystem/CarRentingSystem.Test/Moqs/DatabaseMoq.cs
@@ -0,0 +1,21 @@
+﻿using CarRentingSystem.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CarRentingSystem.Test.Moqs
+{
+    public static class DatabaseMoq
+    {
+        public static CarRentingDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<CarRentingDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .Options;
+
+                return new CarRentingDbContext(dbContextOptions);
+            }
+        }
+    }
+}
---
+++ CarRentingSystem/CarRentingSystem.Test/Services/DealerServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using CarRentingSystem.Data.Models;
+using CarRentingSystem.Service.Dealer;
+using CarRentingSystem.Test.Moqs;
+using Xunit;
+
+namespace CarRentingSystem.Test.Services
+{
+    public class DealerServiceTest
+    {
+        [Fact]
+        public void UserReturnTrueIfDealer()
+        {
+            const string userId = "TestUserId";
+
+            var data = DatabaseMoq.Instance;
+            data.Dealers.Add(new Dealer() { UserId = userId });
+            data.SaveChanges();
+
+            var dealerService = new DealerService(data);
+
+            var result = dealerService.IsDealer(userId);
+
+            Assert.True(result);
+        }
+    }
+}
